e2b290f483faeb2ee62ce4353acc3eeccd823adb
elinaDangol <user@IMAC004.local>
2019-01-24 14:42:16 +0545

    trial

---
+++ app/assets/javascripts/books.js
@@ -0,0 +1,12 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// #rename as book.coffee to book.js
+
+function myFunction() {
+  alert("hi")
+  var x = document.getElementById("test")
+
+  document.getElementById("demo") = x;
+}
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,12 @@
+class BooksController < ApplicationController
+  # layout: book_layout
+  def index
+    @title = "List of Books"
+    
+  end
+  def new
+    
+  end
+  def create
+  end 
+end
